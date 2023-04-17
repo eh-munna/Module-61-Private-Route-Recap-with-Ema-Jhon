9e8309e1d5941dcc1e78c8f075e30f0f15a05f5e
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-04-18 00:57:08 +0200

    61-2 Create Login component and style Login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,60 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-    return (
-        <div>
-            <h2>Login coming sooon</h2>
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-center">
+          <h1 className="text-3xl font-bold">Login now!</h1>
         </div>
-    );
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                required
+                name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                required
+                name="password"
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+            <div>
+              <Link></Link>
+              <h3>
+                Don't have an Account? <Link to="/sign-up">Sign Up Now</Link>{' '}
+              </h3>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
-  return <div>hello signup</div>;
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-center">
+          <h1 className="text-3xl font-bold">Sign Up now!</h1>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                required
+                name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                required
+                name="password"
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-primary">
+                Sign Up
+              </button>
+            </div>
+            <div>
+              <Link></Link>
+              <h3>
+                Already Have an Account? <Link to="/login">Log in</Link>{' '}
+              </h3>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
